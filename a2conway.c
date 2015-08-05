4608cb47345739d443968ecdfb52bc03b434d359
boblaublaw <github@sublimation.org>
2015-08-04 21:48:22 -0700

    added a randomizer for the initial state

--- a2conway.c
+++ a2conway.c
@@ -2,7 +2,7 @@
  * Thanks to reference code provided by Bill Buckels:
  * http://www.appleoldies.ca/azgraphics33/rod33.htm
  */
-#include <stdlib.h>
+#include <stdlib.h>     // srand, rand
 #include <stdio.h>      // printf
 #include <string.h>
 #include <conio.h>      // kbhit, cgetc
@@ -18,6 +18,8 @@ typedef unsigned char   uint8_t;  // 1 byte
 typedef char            int8_t;   // 1 byte
 
 // defines
+#define ROWMASK                 0x00FF
+#define COLMASK                 0xFF00
 #define KEYPRESS_BUF_ADDR       0xC000
 #define KEYCLEAR_BUF_ADDR       0xC010
 #define SS_GRAPHICS_MODE        0xC050
@@ -34,10 +36,14 @@ typedef char            int8_t;   // 1 byte
 #define LORES_PAGE2_BASE        0x800
 
 #define TEXTWINDOW_TOP_EDGE     0x22
+#define RSEED1                  0x4E
+#define RSEED2                  0x4F
 
 // globals:
 /* the base addresses for the 48 scanline pairs */
+/* though we only use the first 20 pairs */
 /* for lores graphics mode 40 x 48 x 16 colors */
+/* we are using 40 x 40 x 16 */
 uint16_t page1[24]={
     0x0400, // triad 0
     0x0480,
@@ -144,7 +150,8 @@ void loclear(uint16_t baseaddr[], uint8_t color)
     // write this value into every memory address:
     for (idx = 0; idx < LORES_ROWS; idx++) {
         memset ((uint8_t *)baseaddr[idx], color, LORES_COLS);
-        printf ("writing %d bytes (%x) into addr %p\n", LORES_COLS, color, baseaddr[idx]);
+        //printf ("writing %d bytes (%x) into addr %p\n", 
+        //          LORES_COLS, color, baseaddr[idx]);
     }
 }
 
@@ -153,30 +160,82 @@ int8_t keypress()
     uint8_t *kp = (uint8_t *)KEYPRESS_BUF_ADDR;
     int8_t  c = kp[0];
 
-    /* read the keyboard buffer    */
-    /* and return 0 if no character is waiting */
-    printf ("key is %d\n", c);
+    // read the keyboard buffer
+    // and return 0 if no character is waiting
     if(c < 128) {
         return 0;
     }
-    printf ("returning %d\n", c);
     return c;
+}
+
+uint16_t get_random_bits(uint8_t count)
+{
+    uint16_t result = rand();
+    uint16_t mask = 1;
+    if (count > 1)
+       mask = mask << (count - 1);
+    mask -= 1;
+    printf ("mask is %04x\n", mask);
+    return result & mask;
+}
+
+void loplot(uint16_t baseaddr[], uint8_t row, uint8_t col, uint8_t color)
+{
+    uint8_t pairrow = row / 2;
+    uint8_t *rowptr = baseaddr[pairrow];
+    if (row & 0x1)
+        color <<= 4;
+    rowptr[col] |= color;
+}
+
+void randomize(uint16_t baseaddr[], uint8_t maxrow, uint16_t count)
+{
+    uint16_t r;
+    uint8_t  row, col;
+
+    printf ("randomization complete. press enter to continue\n");
+    waitforkeypress(CH_ENTER);
+    while (--count) {
+        r = rand();
+        // use the high bit to determine which nibble we turn on
+        row = r & ROWMASK;
+        col = (r & COLMASK) >> 8;
+        row %= (maxrow * 2);
+        col %= 40;
+        //printf("turning on %d,%d with %04x\n", row, col, r);
+        loplot(baseaddr, row, col, 0xf);
+    }
+}
+
+void run(void) 
+{
+
 }
 
 int main()
 {
+    // The Apple zero page has some semi-random garbage
+    // suitable for seeding our PRNG
+    uint16_t seed = PEEK(RSEED1) + PEEK(RSEED2) * 256;
+    srand (seed);
+
     // our program just uses the bottom 4 lines of the display
     gotoxy(0,LORES_ROWS);
     POKE(TEXTWINDOW_TOP_EDGE,LORES_ROWS);
 
-    printf ("built at %s %s\n", __DATE__, __TIME__);
-    printf ("press enter to start\n");
+    printf ("built at %s %s\npress enter to start\n",
+        __DATE__, __TIME__);
     waitforkeypress(CH_ENTER);
 
     gr(SS_PAGE1, SS_MIXED_MODE);
     loclear(page1, TGI_COLOR_BLACK);
     
-    printf ("press enter to end\n");
+    // randomly create critters
+    randomize(page1, LORES_ROWS, 400);
+
+    run();
+    
+    printf ("all done! press enter to end\n");
     waitforkeypress(CH_ENTER);
     POKE(TEXTWINDOW_TOP_EDGE,0);
     softsw(SS_TEXT_MODE);
